866ef77515691ad66a222fa3f2ae6a15f9de65aa
pseudooo <mitchell.wright877@gmail.com>
2022-08-14 22:27:43 +0100

    added ef migrations

--- DigitalWonderlabInterview.Domain/DataContext.cs
+++ DigitalWonderlabInterview.Domain/DataContext.cs
@@ -6,7 +6,10 @@ namespace DigitalWonderlabInterview.Domain;
 public class DataContext : DbContext
 {
     public DataContext(DbContextOptions options) : base(options)
-        { }
+        {
+            Database.EnsureCreated();
+            Database.Migrate();
+        }
 
     public DbSet<FilmEntity> Films { get; set; } = null!;
 }
