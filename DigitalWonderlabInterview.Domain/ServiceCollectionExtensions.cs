2c170e6c18c2c6e364e7c3a0598b0c8de0e46248
pseudooo <mitchell.wright877@gmail.com>
2022-08-14 22:47:35 +0100

    added db connection string

--- DigitalWonderlabInterview.Domain/ServiceCollectionExtensions.cs
+++ DigitalWonderlabInterview.Domain/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using DigitalWonderlabInterview.Domain.Interface;
 using DigitalWonderlabInterview.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DigitalWonderlabInterview.Domain;
@@ -8,6 +9,10 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddDomainDependencies(this IServiceCollection sc)
     {
+        sc.AddDbContext<DataContext>(opts => {
+            opts.UseNpgsql("User ID=wonderlab;Password=averystrongpassword;Host=localhost;Port=5432;Database=wonderlab;Pooling=true;Connection Lifetime=0;");
+        });
+        
         sc.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
 
         return sc;
